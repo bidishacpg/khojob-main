e6127e17d7121206c545194212bbaa0c5ff81448
bidishacpg <136471047+bidishacpg@users.noreply.github.com>
2023-06-15 21:00:40 +0545

    css

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
-  <link rel="stylesheet" type=text/css href=style.css>
+  <link rel="stylesheet" type=text/css href=index.css>
 </head>
 <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
