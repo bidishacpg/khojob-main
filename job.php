120b33e56153a3f4b2b5cd772dcf6bfcd1005c81
nikitalaudari41 <2070241@student.bic.edu.np>
2023-06-16 13:52:46 +0545

    navbar

--- job.php
+++ job.php
@@ -1,2 +1,33 @@
+<!DOCTYPE html>
 <html>
-    </html>
+    <head>
+        <title></title>
+        <title>Bootstrap 4 Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+  <link rel="stylesheet"  href="index.css">
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">Khojob</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <!--dropdown-->
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Job
+        </a>
+        <a class="dropdown-item" href="Education.php">Education</a>
+          <a class="dropdown-item" href="Finance.php">Finance</a>
+          <a class="dropdown-item" href="English.php">English</a>
+        </div>
+      </li>
+      
