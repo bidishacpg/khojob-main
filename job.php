41ef1244bbd943c9d521bbbb48ec88e4ff8d1758
nikitalaudari41 <2070241@student.bic.edu.np>
2023-06-16 16:34:20 +0545

    add job

--- job.php
+++ job.php
@@ -10,7 +10,7 @@
 </head>
 <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <a class="navbar-brand" href="#">Khojob</a>
+  <a class="navbar-brand" href="#">Job</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
@@ -23,11 +23,65 @@
       <!--dropdown-->
       <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Job
+         Category
         </a>
-        <a class="dropdown-item" href="Education.php">Education</a>
-          <a class="dropdown-item" href="Finance.php">Finance</a>
-          <a class="dropdown-item" href="English.php">English</a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="Education">Education</a>
+          <a class="dropdown-item" href="Finance">Finance</a>
+
         </div>
       </li>
       
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Level
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="+2">+2</a>
+          <a class="dropdown-item" href="Bachelor">Bachelor</a>
+          <a class="dropdown-item" href="Master">Master's</a>
+
+
+        </div>
+      </li>
+      </ul>
+    
+    </form>
+  </div>
+</nav>
+<div id="demo" class="carousel slide" data-ride="carousel">
+
+
+    
+     
+     
+      </div>   
+    </div>
+    <div class="carousel-item">
+
+    <section class="jobs" id="jobs">
+  <h1 class="heading">our <span>vacancies</span></h1></h1>
+  <div class="jobs-slider">
+
+  <div class="slide">
+    <img src="img/amazon.jpg" alt=""  width="25%" height="25%">
+    <h3>web development</h3>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis quam consequuntur expedita unde possimus perspiciatis tempora ipsam, consectetur nemo libero maiores sint labore officiis, praesentium rerum aliquam quaerat placeat sed.</p>
+    
+</section>
+     
+    
+   
+ 
+   
+
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.4/dist/jquery.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+
+
+
+
+
+</body>
+</html>
