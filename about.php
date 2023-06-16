95d1db3657bfe6d2d82d95e68a61bda0784b5978
nikitalaudari41 <2070241@student.bic.edu.np>
2023-06-16 16:48:43 +0545

    about us

---
+++ about.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>khojob
+</title>
+<style>
+img {
+  width: 25%;
+}
+</style>
+</head>
+<body>
+khojob is the fastest growing job portal in Nepal. With khoj website and application, you can search and apply for jobs with optimal ease and also have a stress-free application period.
+
+<br>
+<img src="imgg/logo.png" alt="logo" width="222" height="222">
+
+
+
+</body>
+</html>
