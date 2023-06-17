1d359c34660cd6171c06c03cbe814cbf77e5d9fa
bidishacpg <136471047+bidishacpg@users.noreply.github.com>
2023-06-17 17:16:55 +0545

    added job part

--- job.php
+++ job.php
@@ -6,7 +6,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
-  <link rel="stylesheet"  href="index.css">
+  <link rel="stylesheet"  href="job.css">
 </head>
 <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
@@ -20,55 +20,44 @@
       <li class="nav-item active">
         <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
       </li>
-      <!--dropdown-->
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-         Category
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="Education">Education</a>
-          <a class="dropdown-item" href="Finance">Finance</a>
+      <!--options-->
+      <div class="option-box">
+            <div class="dropdown">
+                <select class="option-select" id="category" name="category">
+                    <option>Job Category</option>
+                    <option>Education</option>
+                    <option>Finance</option>
+                    <option>Social</option>
 
-        </div>
+                </select>
+                <select class="option-select" id="job-level" name="job-level">
+                    <option>Job Level</option>
+                    <option>Entry</option>
+                    <option>Mid-senior</option>
+                    <option>Director</option>
+              </select>
+              <select class="option-select" id="job-Types" name="job-Types">
+                    <option>Job Types</option>
+                    <option>Part-time</option>
+                    <option>Full-time</option>
+                    <option>Internship</option>
+              </select>
+              <select id="Location" name="Location"  class="filter-select">
+                    <option>Location</option>
+                    <option>kathmandu</option>
+                    <option>Chitwan</option>
+                    <option>pokhara</option>
+                    <option>butwal</option>
+                 </select>
+
+             </div>
+</div>
       </li>
-      
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Level
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="+2">+2</a>
-          <a class="dropdown-item" href="Bachelor">Bachelor</a>
-          <a class="dropdown-item" href="Master">Master's</a>
-
-
-        </div>
-      </li>
-      </ul>
-    
-    </form>
-  </div>
+</ul>
 </nav>
-<div id="demo" class="carousel slide" data-ride="carousel">
+</body>
+</html>
 
-
-    
-     
-     
-      </div>   
-    </div>
-    <div class="carousel-item">
-
-    <section class="jobs" id="jobs">
-  <h1 class="heading">our <span>vacancies</span></h1></h1>
-  <div class="jobs-slider">
-
-  <div class="slide">
-    <img src="img/amazon.jpg" alt=""  width="25%" height="25%">
-    <h3>web development</h3>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis quam consequuntur expedita unde possimus perspiciatis tempora ipsam, consectetur nemo libero maiores sint labore officiis, praesentium rerum aliquam quaerat placeat sed.</p>
-    
-</section>
      
     
    
