f237c8aa343894da517bd100dcd42cb0ee40ef18
ichhashah <136555463+ichhashah@users.noreply.github.com>
2023-06-19 12:54:56 +0545

    commit

--- job.php
+++ job.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width,initial-scale=1.0">
     <title>Jobs Available</title>
-    <link rel="stylesheet" href="jo.css">
+    <link rel="stylesheet" href="job.css">
 </style>"
 </head>
 <body>
@@ -22,8 +22,30 @@
       </ul>
       <div class="nav-side-heading">
         <div class="nav-icon">
-        <img src="imgg/KHO-JOBT.png" class="logo">
+       
 </div>
 <h3>For Users</h3>
 </div>
+</div>
+
+<div class="nav-title">
+  <h3>Gateway to your dream job</h3>
+</div>
+
+<div class="nav.search">
+  <div class="nav-search-main">
+    <img src="./search.svg" alt="search">
+    <input type="text" placeholder="Skills, Designations, Companies";
+</div>
+
+<div class="nav-search-main nav-2">
+  <img src=./add_location_alt.svg" alt="location">
+  <input type="text" placeholder="location">
+</div>
+
+<button class="nav-search-btn">Search</button>
+
+
+
+
 
