f459c0e7dc32b8e09db74397ad415546166452e0
ichhashah <136555463+ichhashah@users.noreply.github.com>
2023-06-23 12:20:23 +0545

    ff

--- apply.php
+++ apply.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Web Form</title>
-    <link rel="stylesheet" href="apply.css">
+    <title>Web Form</title><link rel="stylesheet" href="apply.css">
+    
 </head>
 <body>
     <div class="container">
