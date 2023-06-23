9fcc60a3e24fb1fdb806ca15deacf995d7845a00
nikitalaudari41 <2070241@student.bic.edu.np>
2023-06-23 11:41:53 +0545

    ss

--- about.php
+++ about.php
@@ -1,62 +1,6 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>About Us Section</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="styles.css">
-    <style>
-    </style>
-</head>
+<!DOCTYPE>
+<html lang="en">
+
+<head> </head>
 <body>
-
-
-    <div class="heading">
-      <h1>Khojob</h1>
-      <p>Gateway through your dreamjob</p>
-</div>
-
-  
-<section class="about-us">
-
-  <div class="content">
-    <h2>Who We Are?</h2>
-    <p>KHOJJOB is a leading online job portal that has gained popularity for its user-friendly interface, extensive job listings, and innovative features.
-      <br> With a mission to connect job seekers and employers seamlessly, KHOJOB offers a comprehensive platform for efficient talent acquisition and job searching.One of the key features of KHOJOB is its advanced search functionality. Job seekers can easily navigate through a vast array of job opportunities by filtering search results based on criteria such as industry, location, experience level, and job type. This enables candidates to find the most relevant positions tailored to their skills and career goals.KHOJOB also emphasizes the importance of personalization. Job seekers can create detailed profiles that showcase their qualifications, work experience, and achievements. </p>
-    
-</div>
-
-<div class="mvv-container">
-  <div class="mvv-block">
- 
-    <div class="content">
-      <h2>Our Mission</h2>
-      <p>Mission
-To efficiently connect great candidates to great companies at all levels while constantly developing both ends of the marketplace in diverse functional areas.</p>
-    </div>
-  </div>
-</div>
-    
-<div class="mvv-container">
-  <div class="mvv-block">
-    <div class="content">
-      <h1>Our Vision</h1>
-      <p>Our vision is to create a dynamic and inclusive job portal that revolutionizes the way job seekers connect with employers and facilitates their career growth.
-      Our job portal will serve as a bridge between job seekers and employers, making it easy for both parties to find their perfect match. We believe in empowering job seekers to take control of their career paths. Our portal will offer comprehensive tools and resources, such as resume builders, skill assessments, and career guidance, to help individuals enhance their employability and make informed decisions about their professional future.
-  </div>
-</div>
-
-<div class="mvv-container">
-  <div class="mvv-block">
-    <div class="content">
-      <h2>Our Values</h2>
-      <p>We prioritize honesty, transparency, and ethical practices in all aspects of our job portal. We prioritize the trust and security of our users' data. We implement robust security measures to protect sensitive information, maintain strict privacy policies, and comply with relevant data protection regulations.
-      We believe in the power of collaboration and partnerships. We actively collaborate with industry experts, recruiters, educational institutions, and other stakeholders to create a vibrant job ecosystem and provide valuable resources to job seekers and employers.
-
-  </div>
-</div>
-
-
-</section>
-</body>
-</html>
+<seccti
