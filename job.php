128e4e923d4375df46459db6f2ed25505c65d6ae
nikitalaudari41 <2070241@student.bic.edu.np>
2023-06-25 12:32:59 +0545

    dfgh

--- job.php
+++ job.php
@@ -32,7 +32,7 @@
         </div>
 <br>
 </nav>
-<<<<<<< HEAD
+
 
  
 <div class="main-cont">
@@ -40,7 +40,6 @@
     <h3>Recomended Job</h3>
 </div>
 <!-- start of jobs 1 section-->
-=======
 <section class="main">
       <div class="row">
         <p>There are more than <span>400</span> Jobs</p>
