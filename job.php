41301611d48acbfaf3e35407c2d024dd844a8177
bidishacpg <136471047+bidishacpg@users.noreply.github.com>
2023-06-26 14:59:16 +0545

    shygHSHJ

--- job.php
+++ job.php
@@ -17,21 +17,12 @@
       <li><a  href="index.php">Home</a> </li>
       <li><a  href="about.php">About us</a> </li>
       <li><a  href="contact.php">Contact us</a> </li>
-      <li><a  href="apply.php">Apply Job</a> </li>
-      <li><a  href="company.php">Post Job</a> </li>
+      <li><a  href="loginuser.php">Apply Job</a> </li>
+      <li><a  href="logincompany.php">Post Job</a> </li>
       
         </ul>
     </nav>
 </header>
-<!--<div class="search-wrapper">
-            <div class="search-box">
-                <div class="search-card">
-                    <input class="search-input" type="text" placeholder="job title or keywords">
-                    <button class="search-button"><a href="login.php">Search</a></button>
-
-                </div>
-            </div>
-        </div> -->
         <div class="jobs-list-container">
           <h2>5 jobs</h2>
           <div class="search_bar">
@@ -57,47 +48,87 @@
               applications.
 </div>
 <a href="#" class="details-btn">More Details</a>
-<span class="positions">4 open positions</span>
+<span class="positions">Fuse pvt ltd</span>
 </div>
 <div class="job">
-            <img src="imgg/fuse.png" alt="">
+            <img src="imgg/asterdio.png" alt="">
+            <h3 class="job-title">It officer</h3>
+            <div class="details">
+              Responsible for designing,developing and maintaining system and
+              applications.
+</div>
+<a href="#" class="details-btn">More Details</a>
+<span class="positions">Aesterdio</span>
+</div>
+<div class="job">
+            <img src="imgg/AIT.jpeg" alt="">
+            <h3 class="job-title">product manager</h3>
+            <div class="details">
+              Responsible for designing,developing and maintaining system and
+              applications.
+</div>
+<a href="#" class="details-btn">More Details</a>
+<span class="positions">AIT</span>
+</div>
+<div class="job">
+            <img src="imgg/grow.jpeg" alt="">
             <h3 class="job-title">Software Engineer</h3>
             <div class="details">
               Responsible for designing,developing and maintaining system and
               applications.
 </div>
 <a href="#" class="details-btn">More Details</a>
-<span class="positions">4 open positions</span>
+<span class="positions">Grow</span>
 </div>
 <div class="job">
-            <img src="imgg/fuse.png" alt="">
-            <h3 class="job-title">Software Engineer</h3>
+            <img src="imgg/miniso.jpg" alt="">
+            <h3 class="job-title">Receptionist</h3>
             <div class="details">
               Responsible for designing,developing and maintaining system and
               applications.
 </div>
 <a href="#" class="details-btn">More Details</a>
-<span class="positions">4 open positions</span>
+<span class="positions">Miniso</span>
 </div>
 <div class="job">
-            <img src="imgg/fuse.png" alt="">
-            <h3 class="job-title">Software Engineer</h3>
+            <img src="imgg/amazon.jpg" alt="">
+            <h3 class="job-title">assistant manager</h3>
             <div class="details">
               Responsible for designing,developing and maintaining system and
               applications.
 </div>
 <a href="#" class="details-btn">More Details</a>
-<span class="positions">4 open positions</span>
+<span class="positions">amazon</span>
 </div>
 <div class="job">
-            <img src="imgg/fuse.png" alt="">
-            <h3 class="job-title">Software Engineer</h3>
+            <img src="imgg/youtube.png" alt="">
+            <h3 class="job-title">Content manager</h3>
             <div class="details">
               Responsible for designing,developing and maintaining system and
               applications.
 </div>
 <a href="#" class="details-btn">More Details</a>
-<span class="positions">4 open positions</span>
+<span class="positions">Youtube</span>
+</div>
+<div class="job">
+            <img src="imgg/tiktok.png" alt="">
+            <h3 class="job-title">Content creater</h3>
+            <div class="details">
+              Responsible for designing,developing and maintaining system and
+              applications.
+</div>
+<a href="#" class="details-btn">More Details</a>
+<span class="positions">tiktok</span>
+</div>
+<div class="job">
+            <img src="imgg/google.webp" alt="">
+            <h3 class="job-title">manager</h3>
+            <div class="details">
+              Responsible for designing,developing and maintaining system and
+              applications.
+</div>
+<a href="#" class="details-btn">More Details</a>
+<span class="positions">google</span>
 </div>
 </div>
 </div>
