70985838ac1f4d699a68c265f82fd2860186335b
bidishacpg <136471047+bidishacpg@users.noreply.github.com>
2023-06-26 13:09:18 +0545

    index and job

--- job.php
+++ job.php
@@ -7,20 +7,22 @@
     <title>khojob</title>
     <link rel="stylesheet" href="job.css">
 </head>
-<body>
-  <section>
-    <nav>
-    <div class="navbar">
-      <div class="nav-heading">khojob</div>
-      <ul>
-        <li><a href="about.php">About us</li>
-        <li><a href="contact.php">Contact Us</a> </li>
-        <li><a href="jobseeker.php">Jobseeker</a>
-        <li><a href="company.php">Company</a>
-      </ul>
-
+<header>
+    <div class="logo">
+      <img src="imgg/KHO-JOBT.png" class="khojob">
 </div>
-
+    <nav>
+      <ul>
+      <!--jobs available-->
+      <li><a  href="index.php">Home</a> </li>
+      <li><a  href="about.php">About us</a> </li>
+      <li><a  href="contact.php">Contact us</a> </li>
+      <li><a  href="apply.php">Apply Job</a> </li>
+      <li><a  href="post.php">Post Job</a> </li>
+      
+        </ul>
+    </nav>
+</header>
 <div class="search-wrapper">
             <div class="search-box">
                 <div class="search-card">
@@ -30,105 +32,8 @@
                 </div>
             </div>
         </div>
-<br>
-</nav>
-<section class="main">
-      <div class="row">
-        <p>There are more than <span>400</span> Jobs</p>
-        <a href="#">See all</a>
-      </div>
+        
 
-      <div class="job_card">
-        <div class="job_details">
-          <div class="img">
-            <img class="image" src="imgg/fuse.png">
-          </div>
-          <div class="text">
-            <h2>Software Engineer</h2>
-            <span>Fuse Pvt Ltd - Junior Post</span> <br>
-            <span>1 days ago</span>
-          </div>
-        </div>
-        <div class="job_salary">
-          <h4>$6.7 - $12.5k /yr</h4>
-          <button>Apply</button>
-        </div>
-      </div>
-      
-      <div class="job_card">
-        <div class="job_details">
-          <div class="img">
-            <img class="image" src="imgg/AIT.jpeg"></i>
-          </div>
-          <div class="text">
-            <h2>JavaScript Developer</h2>
-            <span>AiT- Senior Post</span><br>
-            <span>2 days ago</span>
-          </div>
-        </div>
-        <div class="job_salary">
-          <h4>$8.7 - $13.2k /yr</h4>
-          <button>Apply</button>
-         
-        </div>
-      </div>
-
-      <div class="job_card">
-        <div class="job_details">
-          <div class="img">
-            <img class="image" src="imgg/asterdio.png">
-          </div>
-          <div class="text">
-            <h2>Manager</h2>
-            <span>asterdio- Manager Post</span><br>
-            <span>2 days ago</span>
-          </div>
-        </div>
-        <div class="job_salary">
-          <h4>$11 - $18.5k /yr</h4>
-          <button>Apply</button>
-
-          
-        </div>
-      </div>
-
-      <div class="job_card">
-        <div class="job_details">
-          <div class="img">
-            <img class="image" src="imgg/grow.jpeg">
-          </div>
-          <div class="text">
-            <h2>Programmer</h2>
-            <span>Grow - Junior Post</span><br>
-            <span>3 days ago</span>
-          </div>
-        </div>
-        <div class="job_salary">
-          <h4>$6 - $11.5k /yr</h4>
-          <button>Apply</button>
-        </div>
-      </div>
-
-      <div class="job_card">
-        <div class="job_details">
-          <div class="img">
-            <image class="image" src="imgg/intel.png"></i>
-          </div>
-          <div class="text">
-            <h2>React.js Expert</h2>
-            <span>IntelG - VIP</span><br>
-            <span>4 days ago</span>
-          </div>
-        </div>
-        <div class="job_salary">
-          <h4>$12.5 - $25.5k /yr</h4>
-          <button>Apply</button>
-          
-        </div>
-      </div>
-    </div>
-    </section>
-  </div>
 
 </body>
 </html>
