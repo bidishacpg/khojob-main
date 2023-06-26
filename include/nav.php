f5e1243a0b75f239dcc224913fdafc71d2e3a299
bidishacpg <136471047+bidishacpg@users.noreply.github.com>
2023-06-26 13:09:24 +0545

    b

---
+++ include/nav.php
@@ -0,0 +1,36 @@
+<header>
+    <div class="logo">
+      <img src="imgg/KHO-JOBT.png" class="khojob">
+</div>
+    <nav>
+      <ul>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+         Login
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="loginuser.php">Jobseeker</a>
+          <a class="dropdown-item" href="logincompany.php">Company</a>
+         </div>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Register
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="regseeker.php">Jobseeker</a>
+          <a class="dropdown-item" href="regcompany.php">Company</a>
+         </div>
+      <!--jobs available-->
+      <li><a  href="job.php">Job</a> </li>
+      <!--more-->
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          More
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="about.php">About Us</a>
+          <a class="dropdown-item" href="contact.php">Contact Us</a>
+         </div>
+        </ul>
+    </nav>
+</header>
