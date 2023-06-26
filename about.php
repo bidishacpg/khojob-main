1d6793478ec5793d31769669a57f197eae03dc1d
bidishacpg <136471047+bidishacpg@users.noreply.github.com>
2023-06-26 20:23:51 +0545

    about us done

--- about.php
+++ about.php
@@ -1,78 +1,59 @@
 <!DOCTYPE>
 <html lang="en">
 <head>
-<link rel="stylesheet" href="styles.css">
+<link rel="stylesheet" href="aboutt.css">
  </head>
-<head> <link rel="stylesheet" href="styles.css"> </head>
 <body>
-<secction id="about">
-    <div class="about-1">
-        <h1>ABOUT US</h1>
-        <p>KHOJJOB is a leading online job portal that has gained popularity for its user-friendly interface, extensive job listings, and innovative features.
-With a mission to connect job seekers and employers seamlessly, KHOJOB offers a comprehensive platform for efficient talent acquisition and job searching.One of the key features of KHOJOB is its advanced search functionality. Job seekers can easily navigate through a vast array of job opportunities by filtering search results based on criteria such as industry, location, experience level, and job type. This enables candidates to find the most relevant positions tailored to their skills and career goals.KHOJOB also emphasizes the importance of personalization. Job seekers can create detailed profiles that showcase their qualifications, work experience, and achievements. </p>
-</p>
-
+<header>
+    <div class="logo">
+      <img src="imgg/KHO-JOBT.png" class="khojob">
 </div>
-<div id="about-2">
-    <div class="cotent-box-lg">
-        <div class="container">
-            <div class="row">
-             <div class="col-md-4">
-                <div class="about-item text-center">
-                    <i class="fa fa-book"></i>
-                    <h3>MISSION</h3>
-                    <hr>
-                    <div class="information">
-                    <img src="imgg/mission1.png" >
-                    <p>To efficiently connect great candidates to great companies at all levels while constantly developing both ends of the marketplace in diverse functional areas.
-                 </P>
-</div>
-                </div>
-            </div>
+    <nav>
+      <ul>
+      <!--jobs available-->
+      <li><a  href="index.php">Home</a> </li>
+      <li><a  href="contact.php">Contact us</a> </li>
+      <li><a  href="apply.php">Apply Job</a> </li>
+      
+        </ul>
+    </nav>
+</header>
+<section class="about-us">
+    <div class="about">
+      <img src="imgg/team.webp" class="pic">
+      <div class="text">
+        <h2>About Us</h2>
+        <h5>KHOJOB-perfect way to find <span>Jobs</span></h5>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita natus ad sed harum itaque ullam enim quas, veniam accusantium, quia animi id eos adipisci iusto molestias asperiores explicabo cum vero atque amet corporis! Soluta illum facere consequuntur magni. Ullam dolorem repudiandae cumque voluptate consequatur consectetur, eos provident necessitatibus reiciendis corrupti!</p>
+        <div class="data">
+        <a href="#" class="hire">More Details</a>
         </div>
+      </div>
     </div>
-</div>
-<div class="col-md-4">
-    <div class="about-item text-center">
-                <div class="about-item text-center">
-                    <i class="fa fa-globe"></i>
-                    <h3>VISSION</h3>
-                    <hr>
-                    <div class="information">
-                    <img src="imgg/vision.png">
-                    <p>Our vision is to create a dynamic and inclusive job portal that revolutionizes the way job seekers connect with employers and facilitates their career growth.
-      Our job portal will serve as a bridge between job seekers and employers, making it easy for both parties to find their perfect match. We believe in empowering job seekers to take control of their career paths. Our portal will offer comprehensive tools and resources, such as resume builders, skill assessments, and career guidance, to help individuals enhance their employability and make informed decisions about their professional future.
-
-                 </P>
-</div>
-                </div>
-            </div>
-        </div>
+  </section>
+  <section class="our-vision">
+    <div class="vision">
+      <img src="imgg/team1.webp" class="pic">
+      <div class="text">
+        <h2>Our-Vision</h2>
+        <h5>KHOJOB-perfect way to find <span>Jobs</span></h5>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita natus ad sed harum itaque ullam enim quas, veniam accusantium, quia animi id eos adipisci iusto molestias asperiores explicabo cum vero atque amet corporis! Soluta illum facere consequuntur magni. Ullam dolorem repudiandae cumque voluptate 
+            consequatur consectetur, eos provident necessitatibus reiciendis corrupti!</p>
+      </div>
     </div>
-</div>
-
-<div class="col-md-4">
-    <div class="about-item text-center">
-                <div class="about-item text-center">
-                    <i class="fa fa-globe"></i>
-                    <h3>VALUES</h3>
-                    <hr>
-                    <div class="information">
-                    <img src="imgg/value.webp">
-                    <p>To work for our clients and candidates seeking job with integrity,respect and responsibility and help employeers achieve success through peoples.
-                        
-                 </P>
-</div>
-                </div>
-            </div>
-        </div>
+  </section>
+  <section class="our-goals">
+    <div class="goals">
+      <img src="imgg/goal.jpg" class="pic">
+      <div class="text">
+        <h2>Our-goals</h2>
+        <h5>KHOJOB-perfect way to find <span>Jobs</span></h5>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita natus ad sed harum itaque ullam enim quas, veniam accusantium, quia animi id eos adipisci iusto molestias asperiores explicabo cum vero atque amet corporis! Soluta illum facere consequuntur magni. Ullam dolorem repudiandae cumque voluptate 
+            consequatur consectetur, eos provident necessitatibus reiciendis corrupti!</p>
+      </div>
     </div>
-</div>
-</div>
-</section>
-<footer class="text-center">
-    <p>Copyright&copy; 2023 All rights reserved</p>
-</footer>
+  </section>
+  <?php include_once 'include/footer.php'; ?>
 </body>
 </html>
 
