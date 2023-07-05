10daef1cc7e73772666aa7a63dcb7c3c1fbbd1f8
nikitalaudari41 <2070241@student.bic.edu.np>
2023-07-05 12:19:53 +0545

    nnn

--- regseeker.php
+++ regseeker.php
@@ -12,8 +12,7 @@ $mysqli= mysqli_connect($Host,$Username,$Password,$dbName);
         $email=$_POST['email'];
 		$password = $_POST['password'];
 		
-		// include database connection file
-		include_once("mysqli.php");
+	
 				
 		// Insert user data into table
 		$result = mysqli_query($mysqli, "INSERT INTO reg(username,email,password) VALUES('$username','$email','$password')");
@@ -59,6 +58,7 @@ $mysqli= mysqli_connect($Host,$Username,$Password,$dbName);
     <input type="text" class="input-field" name="username" placeholder="Enter your  Username">
     <input type="email" class="input-field" name ="email" placeholder="Enter your Email" >
     <input type="password" class="input-field" name="password" placeholder="Enter your Password" >
+    <input type=" confirm password" class="input-field" name="confirm password" placeholder="confirm password" >
     <input type="checkbox" class="checkbox"><span>I agree to the terms and conditions </span>
     <button type="submit"  name="Register" class="submit-btn">Register</button>
     <div class="container login">
