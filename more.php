0fad32f943344394297e534e034af068a7b0fa41
nikitalaudari41 <2070241@student.bic.edu.np>
2023-07-08 16:56:24 +0545

    more

---
+++ more.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet"  href="moredetails.css">
+    <title>More details</title>
+</head>
+<header>
+    <div class="logo">
+      <img src="imgg/KHO-JOBT.png" class="khojob">
+</div>
+    <nav>
+      <ul>
+      <!--jobs available-->
+      <li><a  href="index.php">Home</a> </li>
+      <li><a  href="about.php">About us</a> </li>
+      <li><a  href="contact.php">Contact us</a> </li>
+      <li><a  href="loginuser.php">Apply Job</a> </li>
+      <li><a  href="logincompany.php">Post Job</a> </li>
+        </ul>
+    </nav>
+</header>
+<br>
+
+
+</a>
+<body>
+    
+</body>
+</html>
